4da59aac5c58a19ae9ea7cafa9c8cc7c56081d62
llbuel <llbueltmann@gmail.com>
2023-07-24 23:28:35 -0700

    Update interactivity of console

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,62 @@
 
 #include "Interpreter.h"
 
-int main() {
-	bool runLoop = true;
-	
-	while (runLoop) {
-		std::string text;
+bool checkForQuit() {
+	bool quitBool{ false };
+
+	while(!quitBool) {
+		std::string quitInput;
 		
-		std::cout << "=>> ";
-		std::cin >> text;
+		std::cout << "\nQuit program? [Y/N]: ";
+		std::cin >> quitInput;
 
-		if (text == "\n") {
-			continue;
+		if (quitInput == "Y" || quitInput == "y") {
+			std::cout << "\nGoodbye!\n\n";
+			quitBool = true;
+			return true;
 		}
-		else if (text == "quit") {
-			runLoop = false;
-
-			std::cout << "\nGoodbye!" << "\n";
-
-			break;
+		else if (quitInput == "N" || quitInput == "n") {
+			std::cout << "\nReturning to program.\n'quit' is a reserved name in CLAB.\n\n";
+			quitBool = false;
+			return false;
 		}
+		else {
+			quitBool = false;
+		}
+	}
+}
 
+bool consoleInteraction() {
+	std::string text;
+	
+	std::cout << "CLAB>> ";
+	std::cin >> text;
+	
+	if (text == "quit") {
+		bool quitBool = checkForQuit();
+
+		if (quitBool) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	else {
 		Interpreter interpreter(text);
 		std::string result = interpreter.expr();
 
-		std::cout << "  " << text << " = " << result << "\n";
+		std::cout << "\n" << text << " = " << "\n\n    " << result << "\n\n";
+
+		return true;
+	}
+}
+
+int main() {
+	bool runLoop{ true };
+	
+	while (runLoop) {
+		runLoop = consoleInteraction();
 	}
 
 	return 0;
